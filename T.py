9384014eef28c40b634e4ea0a99e0c91da1b0e03
Mas | D7mi <43019044+mainz4ever@users.noreply.github.com>
2019-10-22 01:56:00 +0300

    Finished getAyahsText function

---
+++ T.py
@@ -0,0 +1,40 @@
+# import json
+#
+#
+# T = format(int("241"), "01")
+#
+# print(T)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# # PASS
+# with open(file="test.json", mode='r', encoding='utf-8') as f:
+#     array = json.load(f)
+#
+# arrayl = array['data']['ayahs']
+# # for distro in array:
+# #     print(array[])
+# print(arrayl)
+# # print(array)
+# # p = json.dumps(array, indent=1, separators=(',', ': '))
+
+
+import array as arr
+
+a = arr.array('d', [1.1, 3.5, 4.5])
+print(a)
